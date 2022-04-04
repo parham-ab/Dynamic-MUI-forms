8897026f1b4905527d5387f3393afdeb387a0b3d
Parham Abolghasemi <parhamab17@gmail.com>
2022-04-04 10:58:11 +0430

    first functionality applied

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+// icons
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import SendIcon from "@mui/icons-material/Send";
+import { IconButton } from "@mui/material";
 
 const Form = () => {
-    return (
-        <div>
-            sa
-        </div>
-    );
+  const [data, setData] = useState([{ firstName: "", lastName: "" }]);
+  // changeHandle
+  const changeHandle = (index, e) => {
+    const values = [...data];
+    values[index][e.target.name] = e.target.value;
+    setData(values);
+  };
+  // submitHandler
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("data", data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        {data.map((item, index) => (
+          <div key={index}>
+            <TextField
+              name="firstName"
+              label="FirstName"
+              type="text"
+              size="small"
+              variant="filled"
+              value={item.firstName}
+              onChange={(e) => changeHandle(index, e)}
+            />
+            <TextField
+              name="lastName"
+              label="lastName"
+              type="text"
+              size="small"
+              variant="filled"
+              value={item.lastName}
+              onChange={(e) => changeHandle(index, e)}
+            />
+            <IconButton>
+              <RemoveIcon />
+            </IconButton>
+            <IconButton>
+              <AddIcon />
+            </IconButton>
+          </div>
+        ))}
+        <Button
+          onClick={submitHandler}
+          size="small"
+          type="submit"
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          Send
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Form;
